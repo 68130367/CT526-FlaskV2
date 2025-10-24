625f468352ac498950ba7b078443b5d6fa5977f6
68130367 <68130367@dpu.ac.th>
2025-10-25 01:24:42 +0700

    feat : add 2 more pages for sum and concat

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template
+from flask_cors import CORS
 
 app = Flask(__name__)
+
+CORS(app)
 
 
 @app.route("/")
@@ -24,6 +27,34 @@ def draw(id):
     count = id
     star_pyramid = ["*" * i for i in range(1, id + 1)]
     return render_template("draw.html", count=id, stars=star_pyramid)
+
+
+@app.route("/sum/<xx>/<yy>")
+def sum_numbers(xx, yy):
+    try:
+        num1 = int(xx)
+        num2 = int(yy)
+        zz = num1 + num2
+
+        result_message = f"The result of sum between {xx} and {yy} is {zz}"
+
+        return render_template("sum.html", message=result_message)
+
+    except ValueError:
+        error_message = "You are using miss data type for operation"
+
+        return render_template("sum.html", message=error_message)
+
+    except Exception as e:
+        error_message = f"An unexpected error occurred: {e}"
+        return render_template("sum", message=error_message)
+
+
+@app.route("/concat/<xx>/<yy>")
+def concat_strings(xx, yy):
+    result_concat = xx + yy
+    result_message = f"The result of concat between {xx} and {yy} is {result_concat}"
+    return render_template("sum.html", message=result_message)
 
 
 if __name__ == "__main__":
